f8f8f66bc7d67ea5209a1c0dfa027543f2f41ab6
bafab <bafa@tutanota.de>
2018-04-07 14:48:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-const drivers = ["Milo", "Otis", "Garfield"]
-
-function destructivelyAppendDriver(name) {
-  drivers.push(name);
-}
-
-function destructivelyPrependDriver (name) {
-  drivers.unshift(name);
-}
-
-function destructivelyRemoveLastDriver () {
-  drivers.pop();
-}
-
-function destructivelyRemoveFirstDriver () {
-  drivers.shift();
-}
-
-function appendDriver (name) {
-  return [...drivers, name];
-}
-
-function prependDriver (name) {
-  return [name, ...drivers];
-}
-
-function removeFirstDriver () {
-  return drivers.slice(1);
-}
-
-function removeLastDriver () {
-  return drivers.slice(0, drivers.length - 1);
-}
+const drivers =  ["Milo", "Otis", "Garfield"]
+function destructivelyAppendDriver(name) { drivers.push(name) }
+function destructivelyPrependDriver(name) { drivers.unshift(name) }
+function destructivelyRemoveLastDriver() { drivers.pop() }
+function destructivelyRemoveFirstDriver() { drivers.shift() }
+function appendDriver(name) { return newDrivers = [...drivers, name] }
+function prependDriver(name) { return newDrivers = [name, ...drivers] }
+function removeLastDriver() { return drivers.slice(0, drivers.length-1) }
+function removeFirstDriver() { return drivers.slice(1) }
